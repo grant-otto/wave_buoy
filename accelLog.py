ace0da38c26c3fc40051434ce25b38c36bef0189
Grant Otto <gotto@udel.edu>
2018-11-29 12:11:51 -0500

    added temp sensors on accel log

--- accelLog.py
+++ accelLog.py
@@ -33,7 +33,7 @@ date = datetime.strftime(datetime.utcnow(),'%Y%m%d%H%M%S')
 filename= '/home/pi/wave_data/'+date+'acc_data.csv'
 
 open(filename,"w+")
-line=['Time Stamp', 'Inside Temperature', 'Accelerometer X', 'Accelerometer Y', 'Accelerometer Z', 'Mag X', 'Mag Y', 'Mag Z', 'Gyro X', 'Gyro Y', 'Gyro Z', 'Euler X', 'Euler Y', 'Euler Z', 'Quaternion X', 'Quaternion Y', 'Quaternion Z', 'Lin Acc X', 'Lin Acc Y', 'Lin Acc Z', 'Gravity X', 'Gravity Y', 'Gravity Z']
+line=['Time Stamp','Outside Air Temp', 'Water Temp', 'Inside Temperature', 'Accelerometer X', 'Accelerometer Y', 'Accelerometer Z', 'Mag X', 'Mag Y', 'Mag Z', 'Gyro X', 'Gyro Y', 'Gyro Z', 'Euler X', 'Euler Y', 'Euler Z', 'Quaternion X', 'Quaternion Y', 'Quaternion Z', 'Lin Acc X', 'Lin Acc Y', 'Lin Acc Z', 'Gravity X', 'Gravity Y', 'Gravity Z']
 with open(filename, "a") as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         writer.writerow(line)
@@ -41,28 +41,30 @@ with open(filename, "a") as csv_file:
 '''
 csv indices:
 0: time stamp
-1: inside temperature
-2: acc x
-3: acc y
-4: acc z
-5: mag x
-6: mag y
-7: mag z
-8: gyro x
-9: gyro y
-10: gyro z
-11: Euler x
-12: Euler y
-13: Euler z
-14: quaternion x
-15: quaternion y
-16: quaternion z
-17: linear acceleration x
-18: linear acceleration y
-19: linear acceleration z
-20: gravity x
-21: gravity y
-22: gravity z
+1: outside air temp
+2: water temp
+3: inside temperature
+4: acc x
+5: acc y
+6: acc z
+7: mag x
+8: mag y
+9: mag z
+10: gyro x
+11: gyro y
+12: gyro z
+13: Euler x
+14: Euler y
+15: Euler z
+16: quaternion x
+17: quaternion y
+18: quaternion z
+19: linear acceleration x
+20: linear acceleration y
+21: linear acceleration z
+22: gravity x
+23: gravity y
+24: gravity z
 
 '''
 
@@ -72,23 +74,50 @@ while True:
         read in accelerometer data
         '''
         ##############################
-        line=[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+        line=[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
         line[0]=int(datetime.strftime(datetime.utcnow(),'%Y%m%d%H%M%S'))
-        #line[1]=float('{}'.format(sensor.temperature()))
+        #line[3]=float('{}'.format(sensor.temperature()))
         for i in range(3):
-                line[i+2]=float('{}'.format(sensor.accelerometer[i]))
+                line[i+4]=float('{}'.format(sensor.accelerometer[i]))
         for i in range(3):
-                line[i+5]=float('{}'.format(sensor.magnetometer[i]))
+                line[i+7]=float('{}'.format(sensor.magnetometer[i]))
         for i in range(3):
-                line[i+8]=float('{}'.format(sensor.gyroscope[i]))
+                line[i+10]=float('{}'.format(sensor.gyroscope[i]))
         for i in range(3):
-                line[i+11]=float('{}'.format(sensor.euler[i]))
+                line[i+13]=float('{}'.format(sensor.euler[i]))
         for i in range(3):
-                line[i+14]=float('{}'.format(sensor.quaternion[i]))
+                line[i+16]=float('{}'.format(sensor.quaternion[i]))
         for i in range(3):
-                line[i+17]=float('{}'.format(sensor.linear_acceleration[i]))
+                line[i+19]=float('{}'.format(sensor.linear_acceleration[i]))
         for i in range(3):
-                line[i+20]=float('{}'.format(sensor.gravity[i]))
+                line[i+22]=float('{}'.format(sensor.gravity[i]))
+        #################################
+        '''
+        Read in Thermometers
+        '''
+        #################################
+
+        def read(ds18b20):
+                location = '/sys/bus/w1/devices/' + ds18b20 + '/w1_slave'
+                tfile = open(location)
+                text = tfile.read()
+                tfile.close()
+                secondline = text.split("\n")[1]
+                temperaturedata = secondline.split(" ")[9]
+                temperature = float(temperaturedata[2:])
+                celsius = temperature / 1000
+                return celsius
+
+
+        #air temp
+        airtemp=float("%.3f" % read("28-020691770b3c"))
+
+        #water temp
+        watertemp=float("%.3f" % read("28-020391774c3f"))
+
+        line[1]=airtemp
+        line[2]=watertemp
+        ### Write to a file
 
         with open(filename, "a") as csv_file:
                 writer = csv.writer(csv_file, delimiter=',')
